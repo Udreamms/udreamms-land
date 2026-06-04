d11c5e5797d8f4a440497169f08dcf8432db0108
Udreamms <josep@udreams.com>
2026-06-04 08:50:33 -0600

    chore: change Meta Pixel ID to Udreamms Website Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '27649530617988251');
+fbq('init', '392253097242708');
 fbq('track', 'PageView');
             `,
           }}
@@ -47,7 +47,7 @@ fbq('track', 'PageView');
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=27649530617988251&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=392253097242708&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
